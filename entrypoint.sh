601026df28a957903d3a8dace9c5c32b4bec31e0
Mateo Durante <mateodurante@gmail.com>
2023-10-16 19:40:44 +0200

    avoid reinstalling

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,21 +5,30 @@ actual_dir=$(pwd)
 cd /opt/CTFd/CTFd/plugins/
 IFS=',' read -ra REPOS <<< "$GIT_PLUGINS"
 for repo in "${REPOS[@]}"; do
-  git clone "$repo"
-  cd "$(basename "$repo" .git)"
-  if [ -f "requirements.txt" ]; then
-    pip install -r requirements.txt
+  destination=$(basename "$repo" .git)
+  # If destination exists, skip
+  if [ -d "$destination" ]; then
+    echo "Already installed plugin $repo"
+    continue
   fi
-  cd ..
+  git clone "$repo"
+  if [ -f "$destination/requirements.txt" ]; then
+    pip install -r "$destination/requirements.txt"
+  fi
 done
 
 # Install themes
 cd /opt/CTFd/CTFd/themes/
 # For each var that starts with GIT_THEMES_
 for var in "${!GIT_THEMES_@}"; do
+  # Split the var into an array of 2 elements
   IFS=',' read -ra REPO <<< "${!var}"
-  git clone "${REPO[0]}"
-  mv "$(basename "${REPO[0]}" .git)" "${REPO[1]}"
+  # If destination exists, skip
+  if [ -d "${REPO[1]}" ]; then
+    echo "Already installed theme ${REPO[0]}"
+    continue
+  fi
+  git clone "${REPO[0]}" "${REPO[1]}"
 done
 
 cd "$actual_dir"
